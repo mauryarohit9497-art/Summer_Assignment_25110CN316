18106653fbbf7004d6c60e883da82f2fd2d7e57e
Rohit Lal Maurya <mauryarohit9497@gmail.com>
2026-06-04 16:42:48 +0530

    Added Day1 programs

---
+++ Day1/program1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    int sum = 0;
+
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    cout << "Sum of first " << n << " natural numbers = " << sum << endl;
+
+    return 0;
+}

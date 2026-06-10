969d05b6a2add449540dcb4d873d4c433c9ec0ce
Rohit Lal Maurya <mauryarohit9497@gmail.com>
2026-06-10 18:01:25 +0530

    Added Day7 programs

---
+++ Day7/program28.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int rev = 0;
+
+void reverseNum(int n)
+{
+    if (n == 0)
+        return;
+
+    rev = rev * 10 + (n % 10);
+    reverseNum(n / 10);
+}
+
+int main()
+{
+    int n;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    reverseNum(n);
+
+    cout << "Reversed number = " << rev;
+
+    return 0;
+}
